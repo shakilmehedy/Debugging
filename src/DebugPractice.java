950af2d0fa4df061183f7b1e57b27275553156ae
shakil <shakilmehedy@gmail.com>
2024-11-06 13:07:52 +0600

    ok

---
+++ src/DebugPractice.java
@@ -0,0 +1,7 @@
+public class DebugPractice {
+    public int calculate(int a, int b) {
+        int sum = a + b;
+        int difference = a - b;
+        return sum * difference;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        DebugPractice debugPractice = new DebugPractice();
+        int result = debugPractice.calculate(5, 3);
+        System.out.println("Result: " + result);
+    }
+}
